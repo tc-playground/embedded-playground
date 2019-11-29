79d946bafb8e4c41bbe73911609010fac2cffdfe
Timothy Langford <tim.langford@gmail.com>
2019-11-29 19:52:07 +0000

    Updated boards.

--- _projects/01-blinky-STM324FDISC1/Objects/blinky-main.d
+++
@@ -1,4 +0,0 @@
-.\objects\blinky-main.o: blinky-main.c
-.\objects\blinky-main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\blinky-main.o: C:\Users\timla\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.10.0\Board\Board_LED.h
-.\objects\blinky-main.o: C:\Users\timla\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.10.0\Board\Board_Buttons.h
